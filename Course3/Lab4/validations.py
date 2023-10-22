6e98e37ec466fc377d51c1c10c6a878004dcae85
Christian <carnoult1280@gmail.com>
2023-10-22 11:09:57 +0000

    Added a space in new comment to keep format looking clean and in line with other comments Closes: #1 Updated validations.py python script. Fixed the behavior of validate_user function in validations.py.

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -18,7 +18,7 @@ def validate_user(username, minlen):
     # Usernames can't begin with a number
     if username[0].isnumeric():
         return False
-    #Usernames first character must be a letter
+    # Usernames first character must be a letter
     if not username[0].isalpha():
         return False
     return True
